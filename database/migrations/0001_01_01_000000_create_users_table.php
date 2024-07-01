1231c0b500f77ea2fa64fe649fa7415830c45d65
Teddy K <teddykoerniadi@gmail.com>
2024-07-01 10:53:48 +0700

    publish email component

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -30,6 +30,7 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
+            $table->timestamp('expired_at')->nullable();
             $table->timestamp('created_at')->nullable();
         });
 
